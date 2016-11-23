407533f5389c7eec6916a2d2b0ca1387b5fc1d64
Kenneth LaMantia <kennethlamantia@gmail.com>
2016-11-23 16:08:36 -0500

    feature: Implement method to load data from .csv file.
    In movie_builder module add method load_info that takes movie data
    from .csv file and creates a MovieInfo object for each movie in the
    .csv file.

--- src/movie_trailer_site/movie_builder.py
+++ src/movie_trailer_site/movie_builder.py
@@ -4,6 +4,13 @@ Created on Nov 22, 2016
 @author: kennethalamantia
 '''
 
+import csv
+from movie_trailer_site import movie_info
+
+TITLE = 0
+DESCRIPTION = 1
+TRAILER = 2
+POSTER = 3
 
 class MovieBuilder(object):
     '''Loads information about a list of movies from a .csv file.
@@ -13,7 +20,7 @@ class MovieBuilder(object):
         '''Initializes a dictionary to hold information from
         a parsed .csv file.
         '''
-        self._movie_info = dict()
+        self._movie_info = list()
         self.__csv_file = csv_file_location
         
     def load_info(self):
@@ -21,14 +28,23 @@ class MovieBuilder(object):
         
         Parsed information is stored in self._movie_info.
         '''
-        pass
-    
-    def movie_factory(self):
+        with open(self.__csv_file,"r") as data_file:
+            reader = csv.reader(data_file)
+            # headers in csv file:
+            # title, description, trailer_url, poster_url
+            dummy_header = reader.next()
+            for row in reader:
+                self._movie_info.append(movie_info.MovieInfo(row[TITLE],
+                                                             row[DESCRIPTION],
+                                                             row[TRAILER],
+                                                             row[POSTER])
+                                        )
+            
+    def get_movie_info(self):
         '''Creates MovieInfo objects.
         
         Creates a MovieInfo object for each movie stored in the _movie_info 
         field.
         @return - list of MovieInfo objects.
         '''
-        pass
-    
+        return self._movie_info
